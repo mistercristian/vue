b1491b2292dae705d47505bd10e6ba28a7886eed
mistercristian <cristianguzbo@gmail.com>
2024-11-20 15:42:30 -0500

    Inicio Proyecto

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-createApp(App).mount('#app')
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+
+createApp(App).use(routers).mount('#app')
